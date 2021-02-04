fe491809d57a94f2e6d235160ec2ae5345e8f847
Lucía Martínez <luci.dmartinezr@gmail.com>
2021-02-04 17:45:00 +0100

    fixed apparent leak

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lucimart <lucimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 15:36:31 by lucimart          #+#    #+#             */
-/*   Updated: 2021/02/02 01:53:34 by lucimart         ###   ########.fr       */
+/*   Updated: 2021/02/02 15:03:37 by lucimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static size_t	count_words(const char *s, char c, size_t len)
 	return (words);
 }
 
-static void		split_aux(char **result, char const *s, char c, size_t len)
+static void		split_aux(char **result, char *s, char c, size_t len)
 {
 	int			i;
 	size_t		start;
@@ -57,8 +57,7 @@ static void		split_aux(char **result, char const *s, char c, size_t len)
 		}
 		else
 			end++;
-	result[i] = ft_strnew(1);
-	result[i] = '\0';
+	result[i] = NULL;
 }
 
 char			**ft_split(char const *s, char c)
@@ -67,15 +66,17 @@ char			**ft_split(char const *s, char c)
 	size_t		wcount;
 	char		**result;
 	char		tmp[1];
+	char		*str;
 
 	tmp[0] = c;
-	s = ft_strtrim(s, tmp);
-	if (!s)
+	str = ft_strtrim(s, tmp);
+	if (!str)
 		return (NULL);
-	len = ft_strlen(s);
-	wcount = count_words(s, c, len);
+	len = ft_strlen(str);
+	wcount = count_words(str, c, len);
 	if (!(result = (char **)malloc(sizeof(char *) * (wcount + 1))))
 		return (NULL);
-	split_aux(result, s, c, len);
+	split_aux(result, str, c, len);
+	free(str);
 	return (result);
 }
